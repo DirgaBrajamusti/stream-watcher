135ada3fbec530672c180a1bec09d1d9f47702ed
Dirga Brajamusti <dirgaizanam@gmail.com>
2024-12-31 16:19:12 +0700

    feat: change json for adding task

--- helpers/webserver/types.go
+++ helpers/webserver/types.go
@@ -1,6 +1,6 @@
 package webserver
 
 type JSONAddTask struct {
-	YoutubeUrl string `json:"youtube_url"`
-	OutPath    string `json:"out_path"`
+	YoutubeUrl string `json:"video_url"`
+	OutPath    string `json:"output_directory"`
 }
